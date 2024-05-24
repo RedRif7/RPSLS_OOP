fe3f0d6cac3c7a28d21138b3a9ab974dc9bf7fa7
kristaps <kristaps2114@gmail.com>
2024-05-24 12:53:09 +0300

    changed

--- main.php
+++ main.php
@@ -1,40 +1,51 @@
 <?php
 
 class Element{
-    private string $player;
-    private string $computer;
+    private array $checkWinner;
     private array $elements;
 
-    public function __construct(string $player, array $computer, array $elements)
+    public function __construct()
     {
-        $this->player = strtolower($player);
-        $this->computer = $computer[array_rand($computer)];
-        $this->elements = $elements;
+        $this->checkWinner = [
+            "rock" => ["scissors", "lizard"],
+            "paper" => ["rock", "spock"],
+            "scissors" => ["paper", "lizard"],
+            "lizard" => ["spock", "paper"],
+            "spock" => ["scissors", "rock"]
+        ];
+        $this->elements = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     }
-    public function getWinner(): string{
-        if(!isset($this->elements[$this->player])){
-            return "Error: Write a valid name";
-        }
-        if($this->player == $this->computer){
-            return "It's a draw!\nPlayer - $this->player\nComputer - $this->computer\n";
-        }elseif(in_array($this->player, $this->elements[$this->computer])){
-            return "Computer wins!\nPlayer - $this->player\nComputer - $this->computer\n";
-        }else{
-            return "Player wins!\nPlayer - $this->player\nComputer - $this->computer\n";
+
+    private function computerChoice(): string
+    {
+        return $this->elements[array_rand($this->elements)];
+    }
+
+    private function playerChoice(): string
+    {
+        $playerChoice = strtolower(trim((string)readline("Enter your choice - Rock, Paper, Scissors, Lizard or Spock: ")));
+        if (in_array($playerChoice, $this->elements)) {
+            return $playerChoice;
+        } else {
+            echo "Error, please enter a valid choice\n";
+            return $this->playerChoice();
         }
     }
 
+    public function getWinner(): string
+    {
+        $playerChoice = $this->playerChoice();
+        $computerChoice = $this->computerChoice();
+
+        if ($playerChoice == $computerChoice) {
+            return "It's a draw!\nPlayer - $playerChoice\nComputer - $computerChoice\n";
+        } elseif (in_array($playerChoice, $this->checkWinner[$computerChoice])) {
+            return "Computer wins!\nPlayer - $playerChoice\nComputer - $computerChoice\n";
+        } else {
+            return "Player wins!\nPlayer - $playerChoice\nComputer - $computerChoice\n";
+        }
+    }
 }
 
-$checkWinner = [
-    "rock" => ["scissors", "lizard"],
-    "paper" => ["rock","spock"],
-    "scissors" => ["paper","lizard"],
-    "lizard" => ["spock","paper"],
-    "spock" => ["scissors","rock"]
-];
-$elements = ['rock','paper','scissors','lizard','spock'];
-$playerChoice = (string) readline("Enter your choice - Rock, Paper, Scissors, Lizard or Spock: ");
-$match = new Element($playerChoice, $elements, $checkWinner);
+$match = new Element();
 echo $match->getWinner();
-
